2282e6b2f7cf2646117aa1a4ad55aa17183087a1
Nguyễn Việt Hà <vietha28@Nguyens-MacBook-Pro.local>
2024-10-17 19:13:44 +0700

    AddCate

--- SRMMS/Controllers/CategoriesController.cs
+++ SRMMS/Controllers/CategoriesController.cs
@@ -3,8 +3,8 @@ using Microsoft.EntityFrameworkCore;
 using SRMMS.DTOs;
 using SRMMS.Models;
 
-[ApiController]
 [Route("api/[controller]")]
+[ApiController]
 public class CategoryController : ControllerBase
 {
     private readonly SRMMSContext _context;
@@ -14,11 +14,17 @@ public class CategoryController : ControllerBase
         _context = context;
     }
 
-    [HttpGet("/api/listCategories")]
-    public async Task<ActionResult<IEnumerable<CategoriesDTO>>> GetCategories()
+    [HttpGet("listCategories")]
+    public async Task<ActionResult<IEnumerable<CategoriesDTO>>> GetCategories(int pageNumber = 1, int pageSize = 10)
     {
         
+        var skip = (pageNumber - 1) * pageSize;
+
+        
         var categories = await _context.Categories
+                                       .OrderBy(c => c.CatId) 
+                                       .Skip(skip) 
+                                       .Take(pageSize) 
                                        .Select(c => new CategoriesDTO
                                        {
                                            CatId = c.CatId,
@@ -34,4 +40,57 @@ public class CategoryController : ControllerBase
         
         return Ok(categories);
     }
+
+    [HttpPost("addCategory")]
+    public async Task<ActionResult<CategoriesDTO>> AddCategory(CategoriesDTO categoryDto)
+    {
+        
+        if (await _context.Categories.AnyAsync(c => c.CatName == categoryDto.CatName))
+        {
+            return BadRequest("A category with this name already exists.");
+        }
+
+        
+        var newCategory = new Category
+        {
+            CatName = categoryDto.CatName
+        };
+
+        
+        _context.Categories.Add(newCategory);
+        await _context.SaveChangesAsync();
+
+        
+        var categoryResult = new CategoriesDTO
+        {
+            CatId = newCategory.CatId,
+            CatName = newCategory.CatName
+        };
+
+        
+        return CreatedAtAction(nameof(GetCategoryById), new { catId = newCategory.CatId }, categoryResult);
+    }
+
+    [HttpGet("getCategoryById/{catId}")]
+    public async Task<ActionResult<CategoriesDTO>> GetCategoryById(int catId)
+    {
+        var category = await _context.Categories
+            .Where(c => c.CatId == catId)
+            .Select(c => new CategoriesDTO
+            {
+                CatId = c.CatId,
+                CatName = c.CatName
+            })
+            .FirstOrDefaultAsync();
+
+        if (category == null)
+        {
+            return NotFound();
+        }
+
+        return Ok(category);
+    }
+
 }
+
+
