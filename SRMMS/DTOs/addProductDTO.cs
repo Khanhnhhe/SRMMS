7b909bc6d4cf2ee93455de0deb46e45a2f58c2a5
Nguyễn Việt Hà <vietha28@Nguyens-MacBook-Pro.local>
2024-10-20 12:37:10 +0700

    Fix img

--- SRMMS/DTOs/addProductDTO.cs
+++ SRMMS/DTOs/addProductDTO.cs
@@ -5,8 +5,8 @@
         public string ProductName { get; set; } = null!;
         public string Description { get; set; } = null!;
         public decimal? Price { get; set; }
-        public int? Category  { get; set; } 
-        public IFormFile Image { get; set; } 
+        public int? Category  { get; set; }
+        public string Image { get; set; }
         public string Calories { get; set; } = null!;
         public int? CookingTime { get; set; } 
         public bool? Status { get; set; }
