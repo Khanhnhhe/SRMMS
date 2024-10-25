e80a0d21af8e374010fce8c6b5deb6c72baaa0e6
Nguyễn Việt Hà <vietha28@Nguyens-MacBook-Pro.local>
2024-10-25 17:39:19 +0700

    ListCombo

---
+++ SRMMS/DTOs/ListComboProductDTO.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace SRMMS.DTOs
+{
+	public class ListComboProductDTO
+	{
+        public string ComboName { get; set; } = null!;
+        public string? ComboDescription { get; set; }
+        public string ComboImg { get; set; }
+        public decimal ComboMoney { get; set; }
+        public bool ComboStatus { get; set; }
+    }
+}
+
