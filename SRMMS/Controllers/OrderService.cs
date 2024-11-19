c99b1e61ce9f4558e2ccea42e3b2f89ca0cce3e0
Nguyễn Việt Hà <vietha28@Nguyens-MacBook-Pro.local>
2024-11-19 11:51:42 +0700

    countPro, SearchComboByNameOrPrice, countCb, filterOrderbyTable, searchOrderByTableName,ListOrder

--- SRMMS/Controllers/OrderService.cs
+++ SRMMS/Controllers/OrderService.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Microsoft.AspNetCore.SignalR;
+using Microsoft.EntityFrameworkCore;
 using SRMMS.DTOs;
 using SRMMS.Models;
 
@@ -37,6 +38,116 @@ namespace SRMMS.Controllers
 
             return order.OrderId;
         }
-	}
+        public List<OrderDTO> GetOrders(int pageNumber = 1, int pageSize = 10, string? tableName = null)
+        {
+            var query = _context.Orders
+                .Include(o => o.Table) 
+                .AsQueryable();
+
+            
+            if (!string.IsNullOrEmpty(tableName))
+            {
+               
+                string normalizedTableName = tableName.Replace(" ", "");
+
+                query = query.Where(o => o.Table.TableName.Replace(" ", "").Contains(normalizedTableName)); 
+            }
+
+            var orders = query
+                .Select(o => new OrderDTO
+                {
+                    OrderId = o.OrderId,
+                    TableId = o.TableId,
+                    TableName = o.Table.TableName, 
+                    OrderDate = o.OrderDate,
+                    TotalMoney = o.TotalMoney,
+                    Status = o.Status,
+                    CodeId = o.CodeId,
+                    OrderDetails = o.OrderDetails.Select(od => new OrderDetailDTO
+                    {
+                        ProId = od.ProId,
+                        Quantity = od.Quantiity,
+                        Price = od.Price
+                    }).ToList()
+                })
+                .Skip((pageNumber - 1) * pageSize)  
+                .Take(pageSize)                     
+                .ToList();
+
+            return orders;
+        }
+
+
+        public List<OrderDTO> GetOrdersByTable(int tableId, int pageNumber = 1, int pageSize = 10)
+        {
+            var ordersQuery = _context.Orders
+                .Where(o => o.TableId == tableId)
+                .OrderBy(o => o.OrderDate);
+
+            var pagedOrders = ordersQuery
+                .Skip((pageNumber - 1) * pageSize)
+                .Take(pageSize)
+                .Select(o => new OrderDTO
+                {
+                    OrderId = o.OrderId,
+                    TableId = o.TableId,
+                    OrderDate = o.OrderDate,
+                    TotalMoney = o.TotalMoney,
+                    Status = o.Status,
+                    CodeId = o.CodeId,
+                    OrderDetails = o.OrderDetails.Select(od => new OrderDetailDTO
+                    {
+                        ProId = od.ProId,
+                        Quantity = od.Quantiity,
+                        Price = od.Price
+                    }).ToList()
+                })
+                .ToList();
+
+            return pagedOrders;
+        }
+
+        public List<OrderDTO> SearchOrdersByTableName(string tableName, int pageNumber = 1, int pageSize = 10)
+        {
+            
+            string normalizedTableName = tableName.Replace(" ", "");
+
+            var query = _context.Orders
+                .Include(o => o.Table) 
+                .AsQueryable();
+
+            
+            query = query.Where(o => o.Table.TableName.Replace(" ", "").Contains(normalizedTableName));
+
+            var orders = query
+                .Select(o => new OrderDTO
+                {
+                    OrderId = o.OrderId,
+                    TableId = o.TableId,
+                    TableName = o.Table.TableName, 
+                    OrderDate = o.OrderDate,
+                    TotalMoney = o.TotalMoney,
+                    Status = o.Status,
+                    CodeId = o.CodeId,
+                    OrderDetails = o.OrderDetails.Select(od => new OrderDetailDTO
+                    {
+                        ProId = od.ProId,
+                        Quantity = od.Quantiity,
+                        Price = od.Price
+                    }).ToList()
+                })
+                .Skip((pageNumber - 1) * pageSize)  
+                .Take(pageSize)                     
+                .ToList();
+
+            return orders;
+        }
+
+        public int CountOrders()
+        {
+            
+            return _context.Orders.Count();
+        }
+    }
 }
 
