fd95c81787422fe905e9f15e8aa42c3da6d3740f
Khanhnhhe <160826628+Khanhnhhe@users.noreply.github.com>
2024-11-27 23:01:29 +0700

    point of customer

---
+++ SRMMS/DTOs/AccountPointDTO.cs
@@ -0,0 +1,16 @@
+﻿namespace SRMMS.DTOs
+{
+    public class AccountPointDetailDTO
+    {
+        public int AccountId { get; set; }
+        public string FullName { get; set; }
+        public string Phone { get; set; }
+        public List<PointDetailDTO> Points { get; set; } = new List<PointDetailDTO>();
+    }
+
+    public class PointDetailDTO
+    {
+        public int PointId { get; set; } 
+        public double? Points { get; set; }
+    }
+}
--- SRMMS/DTOs/LoginDTO.cs
+++ SRMMS/DTOs/LoginDTO.cs
@@ -6,6 +6,8 @@
 
         public string Password { get; set; }
 
+        public int AccId { get; set; }
+
 
     }
 }
