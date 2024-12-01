933e541766d21850568bf6b2ba5737afd47ca591
Khanhnhhe <160826628+Khanhnhhe@users.noreply.github.com>
2024-12-01 11:30:20 +0700

    fix end date

--- SRMMS/DTOs/UpdateAccountDTO.cs
+++ SRMMS/DTOs/UpdateAccountDTO.cs
@@ -8,7 +8,6 @@
 
         public bool? Status { get; set; }
 
-        public DateTime? StartDate { get; set; }
-        public DateTime? EndDate { get; set; }
+        //public DateTime? EndDate { get; set; }
     }
 }
